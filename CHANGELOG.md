f129628777f81c9479279269660dedc1c14dbe85
Kevin Purwito <kevin.purwito@gmail.com>
2021-10-28 14:05:49 +0700

    Fix foreign key for rb_items to rb_group_id

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,7 @@ All notable changes to `laravel-rajabiller` will be documented in this file
 
 ## [2.0.1] - 2021-10-28
 - Fix foreign key for rb_orders to rb_item_id
+- Fix foreign key for rb_items to rb_group_id
 
 ## [2.0.0] - 2021-10-28
 - Drop support for PHP7.4
--- database/migrations/02_create_rb_items_table.php
+++ database/migrations/02_create_rb_items_table.php
@@ -18,7 +18,7 @@ class CreateRbItemsTable extends Migration
         if (! Schema::hasTable($tableName)) {
             Schema::create($tableName, function (Blueprint $table) {
                 $table->bigIncrements('id');
-                $table->foreignId('rb_group_id')->nullable();
+                $table->foreignId('rb_group_id')->constrained()->nullable()->onUpdate('CASCADE')->onDelete('CASCADE');
                 $table->boolean('is_active')->index()->default(1);
                 $table->string('code', 20)->unique();
                 $table->string('name')->nullable();
